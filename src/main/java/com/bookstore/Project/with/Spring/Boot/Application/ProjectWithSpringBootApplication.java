68e5569341d96824c25706ced2ebbd6462daa3dd
1Deric1 <santosderic291@gmail.com>
2025-04-01 22:27:31 -0300

    Update Commit BookStore

--- src/main/java/com/bookstore/Project/with/Spring/Boot/Application/ProjectWithSpringBootApplication.java
+++ src/main/java/com/bookstore/Project/with/Spring/Boot/Application/ProjectWithSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.bookstore.Project.with.Spring.Boot;
+package com.bookstore.Project.with.Spring.Boot.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
